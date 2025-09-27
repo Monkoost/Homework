6502e5d2fa994a7ce5369f1d9c0bf6c67bf4f2bc
Monkoost <130833088+Monkoost@users.noreply.github.com>
2025-09-27 22:00:43 +0300

    Add files via upload

---
+++ char-dth.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+ 
+    const char* x = "nigger";
+    int i = 0;
+    while (x[i]!='\0')
+    {
+        i++;
+        std::cout << x[i-1];
+    }
+    std::cout <<" len: "<< i;
+}
+
