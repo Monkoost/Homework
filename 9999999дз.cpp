d54708a3a67c2ee06aa9c604d269225e348af34b
Monkoost <130833088+Monkoost@users.noreply.github.com>
2025-09-28 22:25:57 +0300

    Add files via upload

---
+++ 9999999дз.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int mas[6];
+    int sumTicket=0;
+    for (int i=0;i<=999999;i++)
+    {
+        int temp = i;
+        for (int j = 5;j >= 0;j--)
+        {
+            mas[j] = temp % 10;
+            temp /= 10;
+        }
+        int sum1 = mas[0] + mas[1] + mas[2];
+        int sum2 = mas[3] + mas[4] + mas[5];
+        if (sum1 == sum2)
+        {
+            sumTicket++;
+        }
+
+    }
+    std::cout << sumTicket;
+}
+
